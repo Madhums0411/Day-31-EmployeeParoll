61082511128bea1de653ed0f133ffd35fdabdc2b
madhums0411 <madhums41175@gmail.com>
2022-08-30 20:04:08 +0530

    Create employee payroll tabel

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -1,5 +1,19 @@
 create database Emp_Payroll_Service
 
------datebase is created
+----UC1 datebase is created
 
 use Emp_Payroll_Service
+
+----UC2 Create employee payroll table
+
+create table Emp_Payroll
+(
+Id int identity(1,1) primary key,
+name varchar (100) not null,
+salary money not null,
+start_date date not null
+)
+
+select * from Emp_Payroll
+
+
